53157473532ca18f0d982ce291e53d60e1b3508f
GLAU <GLAU@DESKTOP-OE2RO0N>
2024-02-05 10:57:02 +0530

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim n As Integer
+        n = Console.ReadLine()
+        If n Mod 2 = 0 Then
+            Console.WriteLine("even")
+        Else
+            Console.WriteLine("odd")
+        End If
+        Console.ReadLine()
+    End Sub
+
+End Module
